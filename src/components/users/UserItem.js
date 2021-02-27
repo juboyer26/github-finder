9fe6d188aa6a4c7691fef6cfa197f7ba7c93f886
Justine Boyer <juboyer@student.wethinkcode.co.za>
2021-02-27 10:27:15 +0200

    v1

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -5,7 +5,7 @@ class UserItem extends Component {
         const { login, avatar_url, html_url} = this.props.user;
         return (
             <div className="card text-center">
-               <img src={avatar_url} className="round-img" style={{width:'60px'}}/>
+               <img src={avatar_url} alt="" className="round-img" style={{width:'60px'}}/>
                <h3>{login}</h3>
                <div>
                    <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
